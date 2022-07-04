a45931d6c20c9c4c05de7453a3f76ce307c481df
matthewgani <matthewgani2@gmail.com>
2022-07-04 22:01:05 +0800

    Finish 7.6

--- routed-anecdotes/src/App.js
+++ routed-anecdotes/src/App.js
@@ -79,23 +79,30 @@ const CreateNew = (props) => {
     navigate('/')
   }
 
+  const handleReset = () => {
+    content.reset()
+    author.reset()
+    info.reset()
+  }
+
   return (
     <div>
       <h2>create a new anecdote</h2>
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input {...content}/>
+          <input {...content} reset=''/>
         </div>
         <div>
           author
-          <input {...author}/>
+          <input {...author} reset=''/>
         </div>
         <div>
           url for more info
-          <input {...info} />
+          <input {...info} reset=''/>
         </div>
-        <button>create</button>
+        <button type='submit'>create</button>
+        <button type='reset' onClick={handleReset}>reset</button>
       </form>
     </div>
   )
